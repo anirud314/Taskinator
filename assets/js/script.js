7359d3c591747b4571d31b5c347822e72156bbf7
Aniruddha Bhattacharya <p.aniruddhabhattacharya@gmail.com>
2021-04-17 11:06:23 -0700

    changed javascript to work on form submission rather than button press

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
-var buttonEl = document.querySelector("#save-task");
+/*Replace This: var buttonEl = document.querySelector("#save-task");
+With this:*/
+var formEl = document.querySelector("#task-form");
+
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 //console.log(buttonEl);
 var createTaskHandler = function(){
+    
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+/*Replace this: buttonEl.addEventListener("click", createTaskHandler);
+With this: */
+formEl.addEventListener("submit", createTaskHandler);
